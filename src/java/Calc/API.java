c30bc5b49f701a58bf025d0c46380f3f2fc0b636
GD <gd@gd.com>
2017-03-29 13:33:22 +0100

    1st Commit

---
+++ src/java/Calc/API.java
@@ -0,0 +1,28 @@
+package Calc;
+
+
+public class API {
+
+
+    public float addition(float x, float y) {
+
+        return x + y;
+    }
+
+    public float subtraction(float x, float y) {
+
+        return x - y;
+    }
+
+    public float multiplication(float x, float y) {
+
+        return x * y;
+    }
+
+    public float division (float x, float y) {
+
+        return x / y;
+    }
+
+
+}
